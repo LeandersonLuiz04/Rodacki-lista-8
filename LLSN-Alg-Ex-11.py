4a81b44c97f64d18d81186d27c1c8a9133934107
LeandersonLuiz04 <161379464+LeandersonLuiz04@users.noreply.github.com>
2024-06-05 20:09:44 -0300

    Update LLSN-Alg-Ex-11.py

---
+++ LLSN-Alg-Ex-11.py
@@ -0,0 +1,16 @@
+def descompacta(lista):
+    if not lista:
+        return []
+    valor = lista[0]
+    contagem = lista[1]
+    return [valor] * contagem + descompacta(lista[2:])
+
+def main():
+    lista_codificada = ["A", 12, "B", 4, "A", 6, "B", 1]
+    print("Lista codificada:", lista_codificada)
+    
+    lista_descompactada = descompacta(lista_codificada)
+    print("Lista descompactada:", lista_descompactada)
+
+if __name__ == "__main__":
+    main()
